20870b40db6aef368d616bdb231349420ffecbbb
mariarauj <dudinha.araujo04@gmail.com>
2023-12-03 15:36:03 -0300

    tentativa

--- src/Componentes/navbar.tsx
+++ src/Componentes/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar(){
             <ul className={styles.ul}>
                 <li className={styles.li}>Home</li>
                 <li className={styles.li}>Contatos</li>
-                <li className={styles.li}>Carápio</li>
+                <li className={styles.li}>Cardápio</li>
                 <li className={styles.li}>Entrar</li>
                 <li className={styles.li}>Registrar</li>
                 <img src="url(https://www.figma.com/file/kWyheRCvQQZdxErXk0vXSu/P%C3%A1ginas-projeto?type=design&node-id=23-8&mode=design&t=abClQc5HnUqmnu1t-4)" alt="" />
