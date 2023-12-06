c81510e57bb45a3c1580106d792e2082f86bd260
mariarauj <dudinha.araujo04@gmail.com>
2023-12-06 19:25:43 -0300

    gg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Style from '@/styles/home.module.css'
+import Navbar from '@/Componentes/navbar'
+import Bolotas from './bolotas'
 
 export default function Home() {
   return (
@@ -10,8 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className= {Style.main}>
+      <main>
         <section>
+          <Navbar/>
+          <Bolotas/>
           <h1>Cardápio</h1>
           <div>
             <button>carrinho</button>
