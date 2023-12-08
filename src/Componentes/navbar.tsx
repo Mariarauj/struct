39b4250bce255417d95901d7543442f75ec8a964
mariarauj <dudinha.araujo04@gmail.com>
2023-12-08 10:42:33 -0300

    projeto

--- src/Componentes/navbar.tsx
+++ src/Componentes/navbar.tsx
@@ -1,17 +1,19 @@
 import styles from "@/styles/navbar.module.css"
+import { publicDecrypt } from "crypto"
 
 export default function Navbar(){
     return(
-        <nav className={styles.navbar}>
+        <div className={styles.navbar}>
             <ul className={styles.ul}>
                 <li className={styles.li}>Home</li>
                 <li className={styles.li}>Contatos</li>
                 <li className={styles.li}>Cardápio</li>
                 <li className={styles.li}>Entrar</li>
+                <img className={styles.image}/>
                 <li className={styles.li}>Registrar</li>
             </ul> 
-            <img src='' alt="" />
-        </nav>
+            
+        </div>
         
     )
 }
